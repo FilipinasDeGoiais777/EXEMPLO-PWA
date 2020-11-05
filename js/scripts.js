ad1cc913edb564ab39974e4c46bca73ba451733a
FilipinasDeGoiais777 <61288053+FilipinasDeGoiais777@users.noreply.github.com>
2020-11-05 06:25:02 -0300

    1º Commit

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+    //$.ajax({
+    //dataType: 'JSON',
+    //type: 'GET',
+    //assync: true,
+    //url: 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/35/distritos',
+    //success: function(dados) {
+    //for (var i = 0; i < dados.length; i++) {
+    //$('#cidade').append(`<option value="${dados[i].id}">${dados[i].nome}</option>`)
+    //}
+    //}
+    //})
+
+    $('#cidade').keyup(function() {
+        let nome = $(this).val()
+
+        $.ajax({
+            dataType: 'JSON',
+            type: 'GET',
+            assync: true,
+            url: 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/35/distritos',
+            success: function(dados) {
+                for (var i = 0; i < dados.length; i++) {
+                    if (dados[i].nome == nome) {
+                        $('#cidade').after(`<p>${dados[i].nome}</p>`)
+                    }
+                }
+            }
+        })
+
+    })
+
+    $('.btn-success').click(function(e) {
+
+        e.preventDefault()
+
+        var dados = $('#form-cad').serialize()
+
+        console.log(dados)
+    })
+
+});
