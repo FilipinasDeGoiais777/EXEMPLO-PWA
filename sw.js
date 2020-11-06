f4b564fc5b64f829bbf25b3158166bb6e9b198e9
FilipinasDeGoiais777 <61288053+FilipinasDeGoiais777@users.noreply.github.com>
2020-11-06 11:12:02 -0300

    Commit SW

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
+var CACHE_NAME = 'papa_cache_v1'
+var urlsToCache = [
+    'css/bootstrap.css',
+    'js/bootstrap.js',
+    'js/jquery-3.5.1.js',
+    'img/marca.png',
+    'img/lugar-colocar.png',
+    'libs/sweetalert2/src/sweetalert2.scss'
+]
+
 self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Cache aberto...')
+            return cache.AddAll(urlsToCache)
+        })
+    )
+})
+self.addEventListener('fetch', function(event) {
 
 })
